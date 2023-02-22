a909afa4a3b6fea8f419e9351cbcc28ec93d9d5b
JohannBlood <georg-putilov@mail.ru>
2023-02-22 20:29:35 +0300

    03_MergeRequirements
    +gameplay
    +my_ask
    +my_inform

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,3 +1,17 @@
+from random import choice
+
+
+def my_ask(prompt: str, valid: list[str] = None):
+    while True:
+        a = input(prompt)
+        if not valid or a in valid:
+            return a
+
+
+def my_inform(format_string: str, bulls: int, cows: int):
+    print(format_string.format(bulls, cows))
+
+
 def bullscows(guess: str, secret: str):
     n_bulls = 0
     n_cows = len(set(guess) & set(secret))
@@ -5,6 +19,20 @@ def bullscows(guess: str, secret: str):
         if guess[i] == secret[i]:
             n_bulls += 1
     return n_bulls, n_cows
-            
 
-print(bullscows("ропот", "полип"))
+
+def gameplay(ask: callable, inform: callable, words: list[str]):
+    secret = choice(words)
+    tries = 0
+    while True:
+        guess = ask("Введите слово: ", words)
+        tries += 1
+        b, c = bullscows(guess, secret)
+        inform("Быки: {}, Коровы: {}", b, c)
+        if guess == secret:
+            print(tries)
+            break
+
+
+gameplay(my_ask, my_inform, ['asd', 'wdr'])
+# my_inform("Быки: {}, Коровы: {}", 1, 2)
