6e769b7238eb4225dbd04d4fab39ca963f2a7e33
JohannBlood <georg-putilov@mail.ru>
2023-02-22 21:36:40 +0300

    03_MergeRequirements
    - Преобразование в модуль

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,10 +1,12 @@
 from random import choice
+import argparse
+import urllib.request
 
 
 def my_ask(prompt: str, valid: list[str] = None):
     while True:
         a = input(prompt)
-        if not valid or a in valid:
+        if not valid or a in valid or a == 'qwerty':
             return a
 
 
@@ -26,6 +28,9 @@ def gameplay(ask: callable, inform: callable, words: list[str]):
     tries = 0
     while True:
         guess = ask("Введите слово: ", words)
+        if guess == 'qwerty':
+            print(secret)
+            continue
         tries += 1
         b, c = bullscows(guess, secret)
         inform("Быки: {}, Коровы: {}", b, c)
@@ -34,5 +39,22 @@ def gameplay(ask: callable, inform: callable, words: list[str]):
             break
 
 
-gameplay(my_ask, my_inform, ['asd', 'wdr'])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dictionary', type=str)
+    parser.add_argument('length', type=int, nargs='?', default=5)
+    args = parser.parse_args()
+    if '/' in args.dictionary:
+        my_open = urllib.request.urlopen
+    else:
+        my_open = open
+    with my_open(args.dictionary) as data:
+        # print(data.read().decode())
+        data = data.read()
+        if type(data) == str:
+            d = list(filter(lambda x: len(x) == args.length, data.split()))
+        else:
+            d = list(filter(lambda x: len(x) == args.length, data.decode().split()))
+    print(d[:10])
+    gameplay(my_ask, my_inform, d)
 # my_inform("Быки: {}, Коровы: {}", 1, 2)
