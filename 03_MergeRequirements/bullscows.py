30711d85486807723aa420f7c585261c5c8876b2
JohannBlood <georg-putilov@mail.ru>
2023-02-22 20:04:09 +0300

    03_MergeRequirements
    + def bullscows

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,2 +1,10 @@
 def bullscows(guess: str, secret: str):
-    
+    n_bulls = 0
+    n_cows = len(set(guess) & set(secret))
+    for i in range(len(guess)):
+        if guess[i] == secret[i]:
+            n_bulls += 1
+    return n_bulls, n_cows
+            
+
+print(bullscows("ропот", "полип"))
