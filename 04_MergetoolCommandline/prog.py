45f647714c9e81a1b210c94962e2e45086c706f6
JohannBlood <georg-putilov@mail.ru>
2023-03-09 18:24:38 +0300

    Clean

--- 04_MergetoolCommandline/prog.py
+++ 04_MergetoolCommandline/prog.py
@@ -146,29 +146,3 @@ class Cmdline(cmd.Cmd):
 
 if __name__ == '__main__':
     Cmdline().cmdloop()
-# parser = argparse.ArgumentParser()
-# parser.add_argument('cowsay', default='', type=str, nargs='?')
-# parser.add_argument('-e', '--eye_string', default='oo', type=str)
-# parser.add_argument('-f', '--cowfile', default='')
-# parser.add_argument('-l', default='', action='store_true')
-# parser.add_argument('-T', '--tongue_string', default='', type=str)
-# parser.add_argument('-W', '--column', default=40, type=int)
-# args = parser.parse_args()
-# # print(bool(args.cowfile))
-# if args.l:
-#     print(list_cows())
-# elif not args.cowfile:
-#     print(cowsay(message=args.cowsay, eyes=args.eye_string,
-#                     tongue=args.tongue_string, width=args.column))
-# else:
-#     try:
-#         if '/' in args.cowfile:
-#             with open(args.cowfile) as cowfile:
-#                 print(cowsay(message=args.cowsay, eyes=args.eye_string,
-#                     tongue=args.tongue_string, width=args.column, cowfile=cowfile.read()))
-#         else:
-#             print(cowsay(message=args.cowsay, eyes=args.eye_string,
-#                         tongue=args.tongue_string, width=args.column, cow=args.cowfile))
-#     except FileNotFoundError:
-#             print('No such file or directory')
-#             sys.exit(1)
